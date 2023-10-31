04f3e039a1ef7182766a7043b723b597da27d1ff
Sergen Bakır <53319517+srgnbkr@users.noreply.github.com>
2023-10-31 18:33:58 +0300

    Core Persistence Created

---
+++ src/corePackages/Core.Persistence/Repositories/EfRepositoryBase.cs
@@ -0,0 +1,83 @@
+﻿using Core.Persistence.Paging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Repositories
+{
+    public class EfRepositoryBase<TEntity, TEntityId, TContext> : IAsyncRepository<TEntity, TEntityId>
+    where TEntity : Entity<TEntityId>
+    where TContext : DbContext
+    {
+        protected readonly TContext Context;
+
+        public EfRepositoryBase(TContext context)
+        {
+            Context = context;
+        }
+
+        public IQueryable<TEntity> Query() => Context.Set<TEntity>();
+
+
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            await Context.AddAsync(entity);
+            await Context.SaveChangesAsync();
+            return entity;
+        }
+
+
+        
+
+        public async Task<TEntity> DeleteAsync(TEntity entity, bool permanent = false)
+        {
+            Context.Remove(Context.Entry(entity).State = EntityState.Deleted);
+            await Context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> predicate, Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null, bool withDeleted = false, bool enableTracking = true, CancellationToken cancellationToken = default)
+        {
+            IQueryable<TEntity> queryable = Query();
+            if (!enableTracking)
+                queryable = queryable.AsNoTracking();
+            if (include != null)
+                queryable = include(queryable);
+            if (withDeleted)
+                queryable = queryable.IgnoreQueryFilters();
+            return await queryable.FirstOrDefaultAsync(predicate, cancellationToken);
+        }
+
+        public async Task<IPaginate<TEntity>> GetListAsync(Expression<Func<TEntity, bool>>? predicate = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null, Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null, int index = 0, int size = 10, bool withDeleted = false, bool enableTracking = true, CancellationToken cancellationToken = default)
+        {
+            IQueryable<TEntity> queryable = Query();
+            if (!enableTracking)
+                queryable = queryable.AsNoTracking();
+            if (include != null)
+                queryable = include(queryable);
+            if (withDeleted)
+                queryable = queryable.IgnoreQueryFilters();
+            if (predicate != null)
+                queryable = queryable.Where(predicate);
+            if (orderBy != null)
+                return await orderBy(queryable).ToPaginateAsync(index, size, from: 0, cancellationToken);
+            return await queryable.ToPaginateAsync(index, size, from: 0, cancellationToken);
+        }
+
+       
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            Context.Update(entity);
+            await Context.SaveChangesAsync();
+            return entity;
+        }
+
+      
+
+    }
+}
