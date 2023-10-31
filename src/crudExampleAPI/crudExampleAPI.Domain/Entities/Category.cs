e64b4b9a31d43238e4f88e7d76377cb10a80c569
Sergen Bakır <53319517+srgnbkr@users.noreply.github.com>
2023-10-31 20:15:28 +0300

    Domain Added

---
+++ src/crudExampleAPI/crudExampleAPI.Domain/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crudExampleAPI.Domain.Entities
+{
+    public class Category : Entity<int>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ src/crudExampleAPI/crudExampleAPI.Domain/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crudExampleAPI.Domain.Entities
+{
+    public class Product : Entity<int>
+    {
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
