fcb9c2839b8c8342322e95458f16c4dc05ceadfb
Sergen Bakır <53319517+srgnbkr@users.noreply.github.com>
2023-11-05 00:38:41 +0300

    Add HttpContextAccessor to services

--- src/crudExampleAPI/crudExampleAPI.WebApi/Program.cs
+++ src/crudExampleAPI/crudExampleAPI.WebApi/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddControllers().AddJsonOptions(x =>
 
 builder.Services.AddPersistenceServices(builder.Configuration);
 builder.Services.AddApplicationServices();
+builder.Services.AddHttpContextAccessor();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
